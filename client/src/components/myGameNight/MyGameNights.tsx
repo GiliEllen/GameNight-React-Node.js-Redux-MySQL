e499591d8fec99e7d5d06f658c9dee16f3101672
Gili <gilimena95@gmail.com>
2022-11-20 12:46:11 +0200

    WIP

--- client/src/components/myGameNight/MyGameNights.tsx
+++ client/src/components/myGameNight/MyGameNights.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./../header/Header";
 import NavBar from "./../navbar/NavBar";
+import {FullCalendarReact} from "../calender/fullCalender/FullCalender";
 
 function MyGameNights() {
   return (
@@ -10,6 +11,7 @@ function MyGameNights() {
       <div>your next game is...</div>
       <div>calnder for game nights</div>
       <div>Schdual new game here</div>
+      <FullCalendarReact/>
     </div>
   );
 }
