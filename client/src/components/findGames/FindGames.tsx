3f253c5e403fe4c2ce8c8a69525f32fccfbde636
Gili <gilimena95@gmail.com>
2022-11-24 14:10:13 +0200

    find Games css

--- client/src/components/findGames/FindGames.tsx
+++ client/src/components/findGames/FindGames.tsx
@@ -1,40 +1,51 @@
-import React from 'react'
-import Header from './../header/Header';
-import NavBar from './../navbar/NavBar';
-import { useEffect } from 'react';
-import axios from 'axios';
-import { useState } from 'react';
-import { GameModel } from './../myGames/MyGames';
-import Game from './../myGames/game/Game';
-import { useAppSelector } from '../../app/hooks';
-import { userSelector } from '../../features/loggedInUser/loggedInUser';
+import React from "react";
+import Header from "./../header/Header";
+import NavBar from "./../navbar/NavBar";
+import { useEffect } from "react";
+import axios from "axios";
+import { useState } from "react";
+import { GameModel } from "./../myGames/MyGames";
+import Game from "./../myGames/game/Game";
+import { useAppSelector } from "../../app/hooks";
+import { userSelector } from "../../features/loggedInUser/loggedInUser";
 
 function FindGames() {
-const loggedInUser = useAppSelector(userSelector);
+  const loggedInUser = useAppSelector(userSelector);
   const [allGamesArray, setAllgamesArray] = useState<GameModel[]>([]);
   const userId = loggedInUser?.user_id;
   async function getAllGames() {
     try {
-      const {data} = await axios.post("/api/games/get-all-games-by-user", {userId});
-      const {gamesArray} = data;
+      const { data } = await axios.post("/api/games/get-all-games-by-user", {
+        userId,
+      });
+      const { gamesArray } = data;
       setAllgamesArray(gamesArray);
-
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   }
 
   useEffect(() => {
     getAllGames();
-  }, [])
+  }, []);
   return (
-    <div>
-        <Header/>
-        <NavBar/>
-        <div>Find games to play</div>
-        {allGamesArray.map((game, idx) => {return <Game key={idx} name={game.game_name} img={game.game_img} addable={game.gameAddble}/>; })}
+    <div className="page">
+      <Header />
+      <NavBar />
+      <div className="full_page game_list">
+        {allGamesArray.map((game, idx) => {
+          return (
+            <Game
+              key={idx}
+              name={game.game_name}
+              img={game.game_img}
+              addable={game.gameAddble}
+            />
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
-export default FindGames
+export default FindGames;
--- client/src/components/myGames/game/Game.tsx
+++ client/src/components/myGames/game/Game.tsx
@@ -1,42 +1,59 @@
-import axios from 'axios'
-import React, { FC, useState } from 'react'
-import { useAppSelector } from '../../../app/hooks'
-import { userSelector } from '../../../features/loggedInUser/loggedInUser'
+import axios from "axios";
+import React, { FC, useState } from "react";
+import { useAppSelector } from "../../../app/hooks";
+import { userSelector } from "../../../features/loggedInUser/loggedInUser";
 
 interface GameProps {
-    name: string,
-    img: string,
-    addable: boolean | undefined
+  name: string;
+  img: string;
+  addable: boolean | undefined;
 }
 
-const Game:FC<GameProps> = ({name, img, addable}) => {
-  const [disabled, setDisabled] = useState<boolean>(false)
+const Game: FC<GameProps> = ({ name, img, addable }) => {
+  const [disabled, setDisabled] = useState<boolean>(false);
   const loggedInUser = useAppSelector(userSelector);
-  const userId = loggedInUser?.user_id
+  const userId = loggedInUser?.user_id;
 
-  async function handleAddGameToList(ev:any) {
+  async function handleAddGameToList(ev: any) {
     try {
-      const {data} = await axios.post("/api/games/add-game-to-user", {name, userId});
-      const {results} = data;
-      if(results.affectedRows > 0) {
+      const { data } = await axios.post("/api/games/add-game-to-user", {
+        name,
+        userId,
+      });
+      const { results } = data;
+      if (results.affectedRows > 0) {
         console.log("game added good!");
-        setDisabled(true)
+        setDisabled(true);
       } else {
-        console.log("did not work")
+        console.log("did not work");
       }
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   }
   return (
-    <div className='game_card'>
+    <div className="game_card">
+      <div className="game_card__container">
         <img src={img} alt="" />
         <h2>{name}</h2>
-        {addable && <h3>Would you like to add this game to your owned game list?</h3>}
-        {!disabled && addable && <button onClick={handleAddGameToList}>ADD</button>}
-        {disabled && addable && <button disabled onClick={handleAddGameToList}>ADD</button>}
+      </div>
+      <div className="information">
+        {addable && (
+          <h3>Would you like to add this game to your owned game list?</h3>
+        )}
+        {!disabled && addable && (
+          <button className="button_add" onClick={handleAddGameToList}>
+            ADD
+          </button>
+        )}
+        {disabled && addable && (
+          <button className="button_add" disabled onClick={handleAddGameToList}>
+            ADD
+          </button>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Game
+export default Game;
